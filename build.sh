6d13d06275c40437028077651dd0070952a3b2fb
beshrislambouli <besherislambouley@gmail.com>
2025-02-20 17:25:49 -0500

    add the generated

--- build.sh
+++ build.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+# #!/bin/bash
 
-# List of required packages
-REQUIRED_PKGS="antlr4 libantlr4-runtime-dev"
+# # List of required packages
+# REQUIRED_PKGS="antlr4 libantlr4-runtime-dev"
 
-# Function to check if a package is installed
-check_installed() {
-    dpkg -l | grep -q "^ii  $1 "
-}
+# # Function to check if a package is installed
+# check_installed() {
+#     dpkg -l | grep -q "^ii  $1 "
+# }
 
-# # Ensure the script runs with sudo privileges
-# if [[ $EUID -ne 0 ]]; then
-#     echo "This script must be run as root. Restarting with sudo..."
-#     exec sudo bash "$0" "$@"
-# fi
+# # # Ensure the script runs with sudo privileges
+# # if [[ $EUID -ne 0 ]]; then
+# #     echo "This script must be run as root. Restarting with sudo..."
+# #     exec sudo bash "$0" "$@"
+# # fi
 
-echo "Checking and installing missing dependencies..."
-for pkg in $REQUIRED_PKGS; do
-    if ! check_installed "$pkg"; then
-        echo "Installing $pkg..."
-        apt-get install -y "$pkg"
-    else
-        echo "$pkg is already installed."
-    fi
-done
+# echo "Checking and installing missing dependencies..."
+# for pkg in $REQUIRED_PKGS; do
+#     if ! check_installed "$pkg"; then
+#         echo "Installing $pkg..."
+#         apt-get install -y "$pkg"
+#     else
+#         echo "$pkg is already installed."
+#     fi
+# done
 
-echo "All dependencies are installed. Proceeding with build..."
+# echo "All dependencies are installed. Proceeding with build..."
 
-: ${CMAKE_DIR:=cmake-build}
+# : ${CMAKE_DIR:=cmake-build}
 
-cmake -D CMAKE_BUILD_TYPE=Release -S . -B $CMAKE_DIR
-cmake --build $CMAKE_DIR
+# cmake -D CMAKE_BUILD_TYPE=Release -S . -B $CMAKE_DIR
+# cmake --build $CMAKE_DIR
---
+++ src/DecafGrammar/generated/DecafParserListener.cpp
@@ -0,0 +1,7 @@
+
+// Generated from DecafParser.g4 by ANTLR 4.7.2
+
+
+#include "DecafParserListener.h"
+
+
