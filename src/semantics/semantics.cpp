bd267a8ca9068146e231fc7d0ff644c991c3bd75
beshrislambouli <besherislambouley@gmail.com>
2025-04-03 00:10:09 -0400

    traverse the tree in semantics

--- src/semantics/semantics.cpp
+++ src/semantics/semantics.cpp
@@ -16,35 +16,37 @@ int Semantics::check (std::ifstream& fin, std::ofstream& fout) {
     std::unique_ptr<AST::Program> ast = std::unique_ptr<AST::Program>(builder.visitProgram(tree).as<AST::Program*>());
 
     AST::ASTPrinter printer(fout);
-
-    ast ->accept(printer);
-
-    // ast->accept(*this);
+    ast -> accept(printer);
+    ast -> accept(*this);
+    
     fout << "PASS" << std::endl;
     return 0;
 }
 
 
 void Semantics::visit(AST::Program& node) {
-    for (auto& u: node.import_decls) {
-        u->accept(*this);
+    for (auto& import_decl : node.import_decls ) {
+        import_decl -> accept (*this);
     }
-    for (auto& u : node.field_decls) {
-        u->accept(*this);
+
+    for (auto& field_decl : node.field_decls ) {
+        field_decl -> accept (*this);
     }
-    for (auto& u : node.method_decls) {
-        u->accept(*this);
+
+    for (auto& method_decl : node.method_decls ) {
+        method_decl -> accept (*this);
     }
 }
 
 void Semantics::visit(AST::Import_Decl& node) {
-    node.id -> accept(*this);
+    node.id -> accept (*this);
 }
 
 void Semantics::visit(AST::Field_Decl& node) {
-    node.field_type -> accept(*this);
-    for (auto& u : node.fields) {
-        u->accept(*this);
+    node.field_type -> accept (*this);
+
+    for (auto& field : node.fields) {
+        field -> accept (*this);
     }
 }
 
@@ -60,8 +62,8 @@ void Semantics::visit(AST::Array_Field_Decl& node) {
 void Semantics::visit(AST::Method_Decl& node) {
     node.method_type -> accept(*this);
     node.id -> accept(*this);
-    for (auto& u: node.parameters) {
-        u -> accept(*this);
+    for (auto& parameter: node.parameters) {
+        parameter -> accept(*this);
     }
     node.block -> accept(*this);
 }
@@ -71,130 +73,226 @@ void Semantics::visit(AST::Parameter& node) {
     node.id -> accept(*this);
 }
 
-void Semantics::visit(AST::Block& node) {
-    for (auto& u : node.field_decls) {
-        u -> accept (*this);
-    }
-    for (auto& u : node.statements) {
-        u -> accept (*this);
-    }
+void Semantics::visit(AST::Method_Type& node) {
+    node.type -> accept(*this);
 }
 
 void Semantics::visit(AST::Field_Type& node) {
     node.type -> accept(*this);
 }
 
-void Semantics::visit(AST::Method_Type& node) {
-    node.type -> accept(*this);
+void Semantics::visit(AST::Block& node) {
+    for (auto& field_decl : node.field_decls) {
+        field_decl -> accept (*this);
+    }
+    for (auto& statement : node.statements) {
+        statement -> accept (*this);
+    }
 }
 
 void Semantics::visit(AST::Location_Assign_Op& node) {
-    std::cout << "Location_Assign_Op" << std::endl;
-    node.assign_op -> accept(*this);
+    node.location -> accept (*this);
+    node.assign_op-> accept (*this);
+    node.expr -> accept (*this);
 }
 
 void Semantics::visit(AST::Location_Incr& node) {
-    std::cout << "Location_Incr" << std::endl;
-    node.increment -> accept(*this);
+    node.location  -> accept (*this);
+    node.increment -> accept (*this);
 }
 
 void Semantics::visit(AST::Method_Call_Stmt& node) {
-    std::cout << "Method_Call_Stmt" << std::endl;
-    for (auto& u: node.extern_args){
-        u->accept(*this);
+    node.id -> accept (*this);
+
+    for (auto& extern_arg : node.extern_args) {
+        extern_arg -> accept (*this);
     }
 }
 
 void Semantics::visit(AST::If_Else_Stmt& node) {
-    std::cout << "If_Else_Stmt" << std::endl;
+    node.expr_if -> accept (*this);
+    node.block_then -> accept (*this);
+
+    if (node.block_else) {
+        node.block_else -> accept (*this);
+    }
 }
 
 void Semantics::visit(AST::For_Stmt& node) {
-    std::cout << "For_Stmt" << std::endl;
     node.id -> accept(*this);
     node.expr_init -> accept(*this);
     node.expr_cond -> accept(*this);
     node.for_update -> accept(*this);
 }
 
-void Semantics::visit(AST::While_Stmt& node) {}
-
-void Semantics::visit(AST::Return_Stmt& node) {}
-
-void Semantics::visit(AST::Break_Stmt& node) {}
-
-void Semantics::visit(AST::Continue_Stmt& node) {}
-
-void Semantics::visit(AST::For_Upd_Assign_Op& node) {}
-
-void Semantics::visit(AST::For_Upd_Incr& node) {
-    std::cout << "For_Upd_Incr" << std::endl;
+void Semantics::visit(AST::While_Stmt& node) {
+    node.expr_cond -> accept (*this);
+    node.block -> accept (*this);
 }
 
-void Semantics::visit(AST::Loc_Var& node) {}
+void Semantics::visit(AST::Return_Stmt& node) {
+    if (node.expr) {
+        node.expr -> accept (*this);
+    }
+}
 
-void Semantics::visit(AST::Loc_Array& node) {}
+void Semantics::visit(AST::Break_Stmt& node) {
 
-void Semantics::visit(AST::Minus_Expr& node) {}
+}
 
-void Semantics::visit(AST::Not_Expr& node) {}
+void Semantics::visit(AST::Continue_Stmt& node) {
 
-void Semantics::visit(AST::INT_Expr& node) {}
+}
 
-void Semantics::visit(AST::LONG_Expr& node) {}
+void Semantics::visit(AST::For_Upd_Assign_Op& node) {
+    node.location -> accept (*this);
+    node.assign_Op-> accept (*this);
+    node.expr -> accept (*this);
+}
 
-void Semantics::visit(AST::Mul_Op_Expr& node) {}
+void Semantics::visit(AST::For_Upd_Incr& node) {
+    node.location -> accept (*this);
+    node.increment-> accept (*this);
+}
 
-void Semantics::visit(AST::Add_Op_Expr& node) {}
+void Semantics::visit(AST::Loc_Var& node) {
+    node.id -> accept (*this);
+}
 
-void Semantics::visit(AST::Rel_Op_Expr& node) {}
+void Semantics::visit(AST::Loc_Array& node) {
+    node.id  -> accept (*this);
+    node.expr-> accept (*this);
+}
 
-void Semantics::visit(AST::Eq_Op_Expr& node) {}
+void Semantics::visit(AST::Minus_Expr& node) {
+    node.expr -> accept (*this);
+}
 
-void Semantics::visit(AST::Logic_Op_Expr& node) {}
+void Semantics::visit(AST::Not_Expr& node) {
+    node.expr -> accept (*this);
+}
 
-void Semantics::visit(AST::Loc_Expr& node) {}
+void Semantics::visit(AST::INT_Expr& node) {
+    node.expr -> accept (*this);
+}
 
-void Semantics::visit(AST::Method_Call_Expr& node) {}
+void Semantics::visit(AST::LONG_Expr& node) {
+    node.expr -> accept (*this);
+}
 
-void Semantics::visit(AST::Literal_Expr& node) {}
+void Semantics::visit(AST::Paren_Expr& node) {
+    node.expr -> accept (*this);
+}
 
-void Semantics::visit(AST::Len_Expr& node) {}
+void Semantics::visit(AST::Mul_Op_Expr& node) {
+    node.expr_lhs -> accept (*this);
+    node.bin_op   -> accept (*this);
+    node.expr_rhs -> accept (*this);
+}
 
-void Semantics::visit(AST::Paren_Expr& node) {}
+void Semantics::visit(AST::Add_Op_Expr& node) {
+    node.expr_lhs -> accept (*this);
+    node.bin_op   -> accept (*this);
+    node.expr_rhs -> accept (*this);
+}
 
-void Semantics::visit(AST::Expr_Arg& node) {}
+void Semantics::visit(AST::Rel_Op_Expr& node) {
+    node.expr_lhs -> accept (*this);
+    node.bin_op   -> accept (*this);
+    node.expr_rhs -> accept (*this);
+}
 
-void Semantics::visit(AST::String_Arg& node) {}
+void Semantics::visit(AST::Eq_Op_Expr& node) {
+    node.expr_lhs -> accept (*this);
+    node.bin_op   -> accept (*this);
+    node.expr_rhs -> accept (*this);
+}
+
+void Semantics::visit(AST::Logic_Op_Expr& node) {
+    node.expr_lhs -> accept (*this);
+    node.bin_op   -> accept (*this);
+    node.expr_rhs -> accept (*this);
+}
+
+void Semantics::visit(AST::Loc_Expr& node) {
+    node.location -> accept (*this);
+}
+
+void Semantics::visit(AST::Method_Call_Expr& node) {
+    node.id -> accept (*this);
+    for (auto& extern_arg : node.extern_args) {
+        extern_arg -> accept (*this);
+    }
+}
+
+void Semantics::visit(AST::Literal_Expr& node) {
+    node.literal -> accept (*this);
+}
+
+void Semantics::visit(AST::Len_Expr& node) {
+    node.id -> accept (*this);
+}
+
+void Semantics::visit(AST::Expr_Arg& node) {
+    node.expr -> accept (*this);
+}
+
+void Semantics::visit(AST::String_Arg& node) {
+
+}
 
 void Semantics::visit(AST::Assign_Op& node) {
-    std::cout << node.type << std::endl;
+    
 }
 
 void Semantics::visit(AST::Increment& node) {
-    std::cout << node.type << std::endl;
+    
 }
 
-void Semantics::visit(AST::Mul_Op& node) {}
+void Semantics::visit(AST::Mul_Op& node) {
 
-void Semantics::visit(AST::Add_Op& node) {}
+}
 
-void Semantics::visit(AST::Rel_Op& node) {}
+void Semantics::visit(AST::Add_Op& node) {
 
-void Semantics::visit(AST::Eq_Op& node) {}
+}
 
-void Semantics::visit(AST::Logic_Op& node) {}
+void Semantics::visit(AST::Rel_Op& node) {
 
-void Semantics::visit(AST::Literal& node) {}
+}
 
-void Semantics::visit(AST::Int_Lit& node) {}
+void Semantics::visit(AST::Eq_Op& node) {
 
-void Semantics::visit(AST::Long_Lit& node) {}
+}
 
-void Semantics::visit(AST::Char_Lit& node) {}
+void Semantics::visit(AST::Logic_Op& node) {
 
-void Semantics::visit(AST::Bool_Lit& node) {}
+}
 
-void Semantics::visit(AST::Type& node) {}
+void Semantics::visit(AST::Literal& node) {
 
-void Semantics::visit(AST::Id& node) {}
+}
+
+void Semantics::visit(AST::Int_Lit& node) {
+
+}
+
+void Semantics::visit(AST::Long_Lit& node) {
+
+}
+
+void Semantics::visit(AST::Char_Lit& node) {
+
+}
+
+void Semantics::visit(AST::Bool_Lit& node) {
+
+}
+
+void Semantics::visit(AST::Type& node) {
+
+}
+
+void Semantics::visit(AST::Id& node) {
+
+}
