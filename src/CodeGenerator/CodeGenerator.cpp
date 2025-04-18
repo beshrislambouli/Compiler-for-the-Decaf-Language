f36956497b93756583d43f7de5f9b1162c83a83c
beshrislambouli <besherislambouley@gmail.com>
2025-04-17 20:33:05 -0400

    skeleton

--- src/CodeGenerator/CodeGenerator.cpp
+++ src/CodeGenerator/CodeGenerator.cpp
@@ -1,7 +1,5 @@
 #include "CodeGenerator.h"
-#include "semantics.h"
-#include "LinearBuilder.h"
-#include "LinearPrinter.h"
+
 
 int CodeGenerator::Generate(std::ifstream& fin, std::ofstream& fout) {
 
@@ -15,5 +13,32 @@ int CodeGenerator::Generate(std::ifstream& fin, std::ofstream& fout) {
 
     Linear::PrettyPrinter printer;
     linear_program -> accept (printer); 
+
+    linear_program -> accept (*this);
     return 0;
 } 
+
+
+
+void CodeGenerator::visit(Linear::Program& program) {}
+void CodeGenerator::visit(Linear::Method& method) {}
+void CodeGenerator::visit(Linear::Operand& instr) {}
+void CodeGenerator::visit(Linear::Literal& instr) {}
+void CodeGenerator::visit(Linear::Location& instr) {}
+void CodeGenerator::visit(Linear::Var& instr) {}
+void CodeGenerator::visit(Linear::Arr& instr) {}
+void CodeGenerator::visit(Linear::Instr& instr) {}
+void CodeGenerator::visit(Linear::Statement& instr) {}
+void CodeGenerator::visit(Linear::Binary& instr) {}
+void CodeGenerator::visit(Linear::Unary& instr) {}
+void CodeGenerator::visit(Linear::Assign& instr) {}
+void CodeGenerator::visit(Linear::Helper& instr) {}
+void CodeGenerator::visit(Linear::Push_Scope& instr) {}
+void CodeGenerator::visit(Linear::Pop_Scope& instr) {}
+void CodeGenerator::visit(Linear::Declare& instr) {}
+void CodeGenerator::visit(Linear::Label& instr) {}
+void CodeGenerator::visit(Linear::Method_Call& instr) {}
+void CodeGenerator::visit(Linear::Return& instr) {}
+void CodeGenerator::visit(Linear::Jump& instr) {}
+void CodeGenerator::visit(Linear::J_Cond& instr) {}
+void CodeGenerator::visit(Linear::J_UnCond& instr) {}
--- src/CodeGenerator/CodeGenerator.h
+++ src/CodeGenerator/CodeGenerator.h
@@ -2,9 +2,37 @@
 
 #include <iostream>
 #include <fstream>
-
-
-class CodeGenerator {
+#include "semantics.h"
+#include "Linear.h"
+#include "LinearBuilder.h"
+#include "LinearPrinter.h"
+#include "Assembler.h"
+class CodeGenerator : Linear::Visitor {
+    Assembler assembler;
 public:
     int Generate(std::ifstream& fin, std::ofstream& fout);
+
+
+    void visit(Linear::Program& program) override;
+    void visit(Linear::Method& method) override;
+    void visit(Linear::Operand& instr) override;
+    void visit(Linear::Literal& instr) override;
+    void visit(Linear::Location& instr) override;
+    void visit(Linear::Var& instr) override;
+    void visit(Linear::Arr& instr) override;
+    void visit(Linear::Instr& instr) override;
+    void visit(Linear::Statement& instr) override;
+    void visit(Linear::Binary& instr) override;
+    void visit(Linear::Unary& instr) override;
+    void visit(Linear::Assign& instr) override;
+    void visit(Linear::Helper& instr) override;
+    void visit(Linear::Push_Scope& instr) override;
+    void visit(Linear::Pop_Scope& instr) override;
+    void visit(Linear::Declare& instr) override;
+    void visit(Linear::Label& instr) override;
+    void visit(Linear::Method_Call& instr) override;
+    void visit(Linear::Return& instr) override;
+    void visit(Linear::Jump& instr) override;
+    void visit(Linear::J_Cond& instr) override;
+    void visit(Linear::J_UnCond& instr) override;
 };
