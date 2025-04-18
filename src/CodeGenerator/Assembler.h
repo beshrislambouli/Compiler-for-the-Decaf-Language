329d66b5ef5e4ffa0f996fc289d66de0371a0013
beshrislambouli <besherislambouley@gmail.com>
2025-04-18 00:21:04 -0400

    working on code gen

--- src/CodeGenerator/Assembler.h
+++ src/CodeGenerator/Assembler.h
@@ -1,7 +1,63 @@
 #pragma once
 #include "CodeGenerator.h"
+#include <map>
 
-
-class Assembler {
+class Frame {
 public:
+    std::map<std::string,std::string> loc;
+
+    bool is_def(std::string id) {
+        return loc.find(id) != loc.end();
+    }
+
+    std::string get(std::string id) {
+        return loc[id];
+    }
+    void put(std::string id, std::string loc_){
+        loc [id] = loc_;
+    }
+};
+class Assembler {
+    std::vector<Frame> frame_segs;
+public:
+    std::string asm_code = "";
+    std::string ret;
+
+    void add_instr(std::string instr){
+        asm_code += instr + '\n';
+    }
+
+    void push_scope() {
+        frame_segs.push_back(Frame());
+    }
+    void pop_scope() {
+        frame_segs.pop_back();
+    }
+
+    std::string get(std::string id){
+        for (int i = frame_segs.size () - 1 ; i >= 0 ; i -- ) {
+            if (frame_segs[i].is_def(id)){
+                return frame_segs[i].get(id);
+            }
+        }
+        std::cout << "ERROR get" << std::endl;
+        exit(1);
+    }
+
+    int stack_size = 0;
+    std::string get_next_loc(Linear::Type type) {
+        if (type == Linear::Int) {
+            stack_size += 4;
+        } else {
+            stack_size += 8;
+        }
+        return std::to_string(stack_size) + "(%rbp)";
+    }
+
+    void declare(std::string id, Linear::Type type){
+        frame_segs.back().put(id,get_next_loc(type));
+    }
+    void declare (std::string id, Linear::Type type, std::string size){
+
+    }
 };
--- src/CodeGenerator/CodeGenerator.cpp
+++ src/CodeGenerator/CodeGenerator.cpp
@@ -1,6 +1,27 @@
 #include "CodeGenerator.h"
 
 
+std::string suf(Linear::Type type) {
+    switch (type) {
+        case Linear::Int: return "l";
+        case Linear::Long: return "q";
+        default: return "z"; // Default to quadword
+    }
+}
+
+std::string reg(int reg_num, Linear::Type type) {
+    std::string ret = "%r";
+    if ( reg_num == 0 ){
+        ret += "10";
+    } else {
+        ret += "11";
+    }
+    if (type==Linear::Int) {
+        ret += "d";
+    }
+    return ret;
+}
+
 int CodeGenerator::Generate(std::ifstream& fin, std::ofstream& fout) {
 
     Semantics semantics;
@@ -15,30 +36,63 @@ int CodeGenerator::Generate(std::ifstream& fin, std::ofstream& fout) {
     linear_program -> accept (printer); 
 
     linear_program -> accept (*this);
+    std::cout << assembler.asm_code;
     return 0;
 } 
 
 
 
-void CodeGenerator::visit(Linear::Program& program) {}
-void CodeGenerator::visit(Linear::Method& method) {}
+void CodeGenerator::visit(Linear::Program& program) {
+    for (auto& method : program.methods) {
+        method -> accept(*this);
+    }
+}
+
+void CodeGenerator::visit(Linear::Method& method) {
+    for (auto& instr : method.instrs) {
+        instr->accept(*this);
+    }
+}
 void CodeGenerator::visit(Linear::Operand& instr) {}
-void CodeGenerator::visit(Linear::Literal& instr) {}
+void CodeGenerator::visit(Linear::Literal& instr) {
+    assembler.ret = "$" + instr.id;
+}
 void CodeGenerator::visit(Linear::Location& instr) {}
-void CodeGenerator::visit(Linear::Var& instr) {}
+void CodeGenerator::visit(Linear::Var& instr) {
+    assembler.ret = assembler.get(instr.id);
+}
 void CodeGenerator::visit(Linear::Arr& instr) {}
 void CodeGenerator::visit(Linear::Instr& instr) {}
 void CodeGenerator::visit(Linear::Statement& instr) {}
 void CodeGenerator::visit(Linear::Binary& instr) {}
 void CodeGenerator::visit(Linear::Unary& instr) {}
-void CodeGenerator::visit(Linear::Assign& instr) {}
+void CodeGenerator::visit(Linear::Assign& instr) {
+    std::string code = "";
+    Linear::Type _T = instr.dist->type;
+
+    instr.operands[0]->accept(*this);
+    std::string operand = assembler.ret;
+    assembler.add_instr("mov" + suf(_T) + " " + operand + ", " + reg(0,_T));
+
+    instr.dist->accept(*this);
+    std::string dist = assembler.ret;
+    assembler.add_instr("mov" + suf(_T) + " " + reg(0,_T) + ", " + dist);
+}
 void CodeGenerator::visit(Linear::Helper& instr) {}
-void CodeGenerator::visit(Linear::Push_Scope& instr) {}
-void CodeGenerator::visit(Linear::Pop_Scope& instr) {}
-void CodeGenerator::visit(Linear::Declare& instr) {}
+void CodeGenerator::visit(Linear::Push_Scope& instr) {
+    assembler.push_scope();
+}
+void CodeGenerator::visit(Linear::Pop_Scope& instr) {
+    assembler.pop_scope();
+}
+void CodeGenerator::visit(Linear::Declare& instr) {
+    assembler.declare(instr.location->id,instr.location->type);
+}
 void CodeGenerator::visit(Linear::Label& instr) {}
 void CodeGenerator::visit(Linear::Method_Call& instr) {}
 void CodeGenerator::visit(Linear::Return& instr) {}
 void CodeGenerator::visit(Linear::Jump& instr) {}
 void CodeGenerator::visit(Linear::J_Cond& instr) {}
-void CodeGenerator::visit(Linear::J_UnCond& instr) {}
+void CodeGenerator::visit(Linear::J_UnCond& instr) {
+
+}
